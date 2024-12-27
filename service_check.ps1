ad6ed6daa3b098a378170f277de1daa15b2cb3f3
OnkarShinde <shindeo522@gmail.com>
2024-12-27 17:58:38 +0530

    Day01

---
+++ service_check.ps1
@@ -0,0 +1,8 @@
+$SERVICES_Name="vds"
+$SERVICES=Get-Service -Name $SERVICES_Name
+
+if ($SERVICES.Status -eq "Running"){
+    Write-Host "$SERVICES_Name is running."
+} else {
+    Write-Host "$SERVICES_Name is not running."
+}
